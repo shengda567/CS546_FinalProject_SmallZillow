f671c30c5cd25e8bd963b78945f2799bcc5543c9
Yun Yang <yy763831702@gmail.com>
2020-11-06 16:28:39 +0800

    1. Added database for comments, users and posts. 2. Added a seed file for generating data 3. Added routes for posts/ and post/:id 4. Added pages for posts/ and posts/:id

---
+++ data/index.js
@@ -0,0 +1,10 @@
+const postsData = require('./posts');
+const usersData = require('./users');
+const commentsData = require('./comments');
+
+
+module.exports = {
+  users: usersData,
+  posts: postsData,
+  comments: commentsData
+};
