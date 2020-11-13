b824b648c7d95e1f8ff0debe97a6e0da919f5d12
LavinWong <72814415+LavinWong@users.noreply.github.com>
2020-11-13 08:29:12 -0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const postRoutes = require('./posts');
 const userRoutes = require('./users');
 const privateRoutes = require('./private');
-
+const registerData = require('./register');
 //const commentRoutes = require('./comments');
 
 const constructorMethod = (app) => {
     app.use('/', userRoutes);
     app.use('/posts', postRoutes);
     app.use('/private', privateRoutes);
+    app.use('/register', registerData);
   //app.use('/comments', commentRoutes);
   
   app.use('*', (req, res) => {
