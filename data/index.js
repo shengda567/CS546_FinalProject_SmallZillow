7913921bfb198d477cf20b7514b7e2599963db41
LavinWong <72814415+LavinWong@users.noreply.github.com>
2020-11-13 08:44:31 -0800

    Add files via upload

--- data/index.js
+++ data/index.js
@@ -1,10 +1,11 @@
 const postsData = require('./posts');
 const usersData = require('./users');
 const commentsData = require('./comments');
-
+const registerData = require('./register');
 
 module.exports = {
   users: usersData,
   posts: postsData,
-  comments: commentsData
+  comments: commentsData,
+  register: registerData
 };
