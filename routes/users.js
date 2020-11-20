d89552cc53260521536edb170fe2a0a2f2eef062
LavinWong <72814415+LavinWong@users.noreply.github.com>
2020-11-21 01:45:21 +0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 const axios = require('axios');
 const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users;
+const pic = require('../data/VerificationCode');
 
 router.get('/', async (req, res) => {
   //res.json({ route: '/users', method: req.method });
@@ -20,37 +21,71 @@ router.post('/', async (req, res) => {
   res.json({ route: '/users', method: req.method });
 });
 
+router.get('/api/getCaptcha', function(req, res, next) {
+  let p = 'ABCDEFGHKMNPQRSTUVWXYZ1234567890';
+  var str = '';
+  for (var i = 0; i < 4; i++)
+  { 
+    str += p.charAt(Math.random() * p.length | 0);
+  }
+  res.cookie('captcha', str); 
+  let img = pic.makeCapcha(str);
+  res.setHeader('Content-Type', 'image/bmp');
+  res.end(img.getFileData());
+  
+})
+
 router.post('/login', async (req, res) => {
 
 	const { username, password } = req.body;
     var check = false;
 
     try{
-
-        for(var i in users)
-        {
-            if(users[i].username == username)
+	const cookies = req.headers.cookie;
+        var list = cookies.split("; ");
+        for(var i = 0; i < list.length; i++) 
+	{
+            var arr = list[i].split("=");   
+            if(arr[0] == 'captcha')
             {
-                check = true;
-                var match = false;
-                match = bcrypt.compareSync(password, users[i].hashedPassword);
-                if(match){
-
-                    res.redirect('/private');
-                }
-                else {
-                    res.status(401).render('pages/error', {error: "Either username or password are error."});
+                 var captcha = arr[1];
+            }
+                
+        }
+	if(authcode == captcha)
+        {
+           
+            for(var i in users)
+            {
+                if(users[i].username == username)
+                {
+                    check = true;
+                    var match = false;
+                    match = bcrypt.compareSync(password, users[i].hashedPassword);
+                    if(match){
+                        req.session.user = { userId: users[i]._id, username: users[i].username, firstName: users[i].firstName, lastName: users[i].lastName, Profession: users[i].Profession, Bio: users[i].Bio };
+                        res.redirect('/private');
+                    }
+                    else {
+                        res.status(401).render('handlebars/error', {error: "Either username or password are error."});
+                    }
                 }
             }
+            if(check == false)
+            {
+                res.status(401).render('handlebars/error', {error: "Username is not exist."});
+            }   
+        
         }
-        if(check == false)
+        else
         {
-            res.status(401).render('pages/error', {error: "Username is not exist."});
+            
+            res.status(401).render('handlebars/error', {error: "Code is wrong."});
         }
     }catch(e){
-    res.status(401).render('pages/error', {error: e});
+    res.status(401).render('handlebars/error', {error: e});
   }
-
+	
 });
 
 router.get('/logout', async (req, res) => {
