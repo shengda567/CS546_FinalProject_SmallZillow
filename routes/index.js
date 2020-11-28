771cb520fe8097974bdcf10dd5f8ea2649a61bf2
LavinWong <72814415+LavinWong@users.noreply.github.com>
2020-11-28 08:35:19 -0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,31 @@
-const postRoutes = require('./posts');
-const userRoutes = require('./users');
+const postRoutes = require("./posts");
+const userRoutes = require("./users");
+const searchRoutes = require("./search");
+
 //const privateRoutes = require('./private');
-const registerData = require('./register');
+const registerData = require("./register");
 //const commentRoutes = require('./comments');
 
 const constructorMethod = (app) => {
-    app.use('/', userRoutes);
-    app.use('/posts', postRoutes);
-    //app.use('/private', privateRoutes);
-    app.use('/register', registerData);
+
+  app.get("/", function (req, res) {
+    res.render("pages/mainPage");
+  });
+  app.use("/search", searchRoutes);
+  app.use("/posts", postRoutes);
+  //app.use('/private', privateRoutes);
+  app.use("/register", registerData);
+  app.use("/login", userRoutes);
   //app.use('/comments', commentRoutes);
-  
-  app.use('*', (req, res) => {
+
+  app.get("/newpost", async (req, res) => {
+    res.render("pages/newpost");
+  });
+
+  app.get("/newpost/success", async (req, res) => {
+    res.render("pages/newpostsuc");
+  });
+  app.use("*", (req, res) => {
     res.sendStatus(404);
   });
 };
