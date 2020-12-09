92a3e0d7043bee783f327b1dfe835f69f3bc1d86
Yun Yang <yy763831702@gmail.com>
2020-12-09 17:41:04 +0800

    made some changes to singlePost page

--- public/js/singlePost.js
+++ public/js/singlePost.js
@@ -1,9 +1,33 @@
-$('#single-container').ready(function(){
+window.onload = function () {
   var commentForm = $('#comment_form');
   var commentsArea = $('#comments-area');
   var commentInput = $('#comment_input');
   var commentError = $('#comment-error');
   var postId = $('#postId').attr("data-id");
+  var recommendPosts = $('#recommendPosts-area');
+  var address = $('#address').attr("data");
+  var state = $('#state').attr("data");
+  var city = $('#city').attr("data");
+  var zipcode = $('#zipcode').attr("data");
+
+    var requestConfig1 = {
+        method: 'POST',
+        url: '/api/similarPosts' ,
+        contentType: 'application/json',
+        data:JSON.stringify({
+          zipcode:zipcode,
+          address:address,
+          city:city,
+          state:state
+      })
+    };
+
+    $.ajax(requestConfig1).then(function (responseMessage) {
+        let newElement = $(responseMessage);
+
+        recommendPosts.append(newElement);
+    });
+
   commentForm.submit(function (event) {
     event.preventDefault();
     let comment_input = commentInput.val();
@@ -32,4 +56,4 @@ $('#single-container').ready(function(){
 
     });
   });
-});
+}
