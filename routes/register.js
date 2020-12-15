2a2e683208079c41b95cacd15d731d6df8baf3d2
LavinWong <72814415+LavinWong@users.noreply.github.com>
2020-12-15 06:41:55 -0800

    add verification code

--- routes/register.js
+++ routes/register.js
@@ -4,11 +4,24 @@ const router = express.Router();
 const data = require("../data");
 const customerinf = data.register;
 var bodyParser = require("body-parser");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
+const pic = require("../data/VerificationCode");
 //const { default: renderEmpty } = require('antd/lib/config-provider/renderEmpty');
 
 router.get("/", async (req, res) => {
   res.render("pages/register");
+});
+
+router.get("/api/getCaptcha", function (req, res, next) {
+    let p = "ABCDEFGHKMNPQRSTUVWXYZ1234567890";
+    var str = "";
+    for (var i = 0; i < 4; i++) {
+        str += p.charAt((Math.random() * p.length) | 0);
+    }
+    res.cookie("captcha", str);
+    let img = pic.makeCapcha(str);
+    res.setHeader("Content-Type", "image/bmp");
+    res.end(img.getFileData());
 });
 
 router.post("/", async (req, res) => {
@@ -16,131 +29,134 @@ router.post("/", async (req, res) => {
   //const personalinf = req.body;
   console.log(personalinf);
   if (!req.body) {
-    res.status(400).json({ error: "You must provide body" + personalinf });
+    res.status(400).render("pages/error",{ error: "You must provide body" + personalinf });
     return;
   }
   if (!personalinf.username) {
-    res.status(400).json({ error: "You must provide post username" });
+    res.status(400).render("pages/error",{ error: "You must provide post username" });
     return;
   }
-  if (!personalinf.firstname) {
-    res.status(400).json({ error: "You must provide post first name" });
+  if (!personalinf.user.firstname) {
+    res.status(400).render("pages/error",{ error: "You must provide post first name" });
     return;
   }
-  if (!personalinf.lastname) {
-    res.status(400).json({ error: "You must provide post last name" });
+  if (!personalinf.user.lastname) {
+      res.status(400).render("pages/error",{ error: "You must provide post last name" });
     return;
   }
   if (!personalinf.email) {
-    res.status(400).json({ error: "You must provide post email" });
+      res.status(400).render("pages/error",{ error: "You must provide post email" });
     return;
   }
   if (!personalinf.gender) {
-    res.status(400).json({ error: "You must provide gender" });
+      res.status(400).render("pages/error",{ error: "You must provide gender" });
     return;
   }
-  if (!personalinf.city) {
-    res.status(400).json({ error: "You must provide city" });
+  if (!personalinf.address.city) {
+      res.status(400).render("pages/error",{ error: "You must provide city" });
     return;
   }
-  if (!personalinf.state) {
-    res.status(400).json({ error: "You must provide state" });
+  if (!personalinf.address.state) {
+      res.status(400).render("pages/error",{ error: "You must provide state" });
     return;
   }
-  if (!personalinf.birthdaytime) {
-    res.status(400).json({ error: "You must provide birthday" });
+  if (!personalinf.BOD) {
+      res.status(400).render("pages/error",{ error: "You must provide birthday" });
     return;
   }
   if (!personalinf.phone) {
-    res.status(400).json({ error: "You must provide phone number" });
+      res.status(400).render("pages/error",{ error: "You must provide phone number" });
     return;
   }
   if (!personalinf.password) {
-    res.status(400).json({ error: "You must provide password" });
+    res.status(400).render("pages/error",{ error: "You must provide password" });
     return;
   }
   if (personalinf.password.length < 10) {
-    res.status(400).json({ error: "You password must has at last 10 digits" });
+    res.status(400).render("pages/error",{ error: "You password must has at last 10 digits" });
+    return;
+  }
+  if (personalinf.verifiy == captcha) {
+    res.status(401).render("pages/error", { error: "Code is wrong." });
     return;
   }
 
   try {
     const users = await customerinf.getAllUsers();
     let match = 0;
-    for(let i in users){
-      if(users[i].username == personalinf.username){
+    for (let i in users) {
+      if (users[i].username == personalinf.username) {
         match = 1;
       }
     }
-    if(match == 0){
+    if (match == 0) {
       const newPost = await customerinf.createaccount(
         personalinf.username,
-        {firstname: personalinf.firstname,
-        lastname: personalinf.lastname},
+        {
+          firstname: personalinf.user.firstname,
+          lastname: personalinf.user.lastname,
+        },
         personalinf.email,
         personalinf.gender,
-        personalinf.city + ', ' + personalinf.state,
-        personalinf.birthdaytime,
+        personalinf.address.city + ", " + personalinf.address.state,
+        personalinf.BOD,
         personalinf.phone,
         personalinf.password
-        );
-        req.session.user = {
-          userId: newPost._id.toString(),
-          username: personalinf.username,
-          firstName: personalinf.firstname,
-          lastName: personalinf.lastname,
-        };
-        const output = `
+      );
+      req.session.user = {
+        userId: newPost._id.toString(),
+        username: personalinf.username,
+        firstName: personalinf.user.firstname,
+        lastName: personalinf.user.lastname,
+      };
+      const output = `
     <p>You create a new account</p>
     
 `;
 
 
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.qq.com',
-            port: 587,
-            secure: false,
-            auth: {
-                user: '1648271784@qq.com',
-                pass: 'wyeqgnsoxracfcej'
-            },
-            tls: {
-                rejectUnauthorized: false
-            }
-        });
+      let transporter = nodemailer.createTransport({
+        host: "smtp.qq.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "1648271784@qq.com",
+          pass: "wyeqgnsoxracfcej",
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      });
 
+      let mailOptions = {
+        from: "1648271784@qq.com",
+        to: req.body.email,
+        subject: "Create a new account",
+        text: "Hello world?",
+        html: output,
+      };
 
-        let mailOptions = {
-            from: '1648271784@qq.com',
-            to: req.body.email,
-            subject: 'Create a new account',
-            text: 'Hello world?',
-            html: output
-        };
-
-
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-            
-        });
-        res.redirect("/login/" + newPost._id.toString());
-    }
-    else{
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      });
+      res.redirect("/users/login" + newPost._id.toString());
+    } else {
       res.json({ message: "username already exist." });
     }
-
   } catch (e) {
-    console.log(e)
+    console.log(e);
     res.status(500).json({ error: e });
   }
 });
 
 router.get("/:id", async (req, res) => {
+  if(!req.params.id){
+    throw 'No id';
+  }
   try {
     const post = await customerinf.getbyone(req.params.id);
     res.json(post);
@@ -150,6 +166,12 @@ router.get("/:id", async (req, res) => {
 });
 
 router.patch("/:id", async (req, res) => {
+  if(!req.params.id){
+    throw 'No id';
+  }
+  if(!req.body){
+    throw 'body wrong';
+  }
   const requestBody = req.body;
   let updatedObject = {};
   try {
